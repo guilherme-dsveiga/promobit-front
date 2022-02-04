3932a26e057f17b46dac4a07a5dea714b957624e
guilherme-dsveiga <guilherme.dsveiga@gmail.com>
2022-02-04 12:58:34 -0300

    Adicionando link na logo do header

--- shared/header/index.js
+++ shared/header/index.js
@@ -1,10 +1,14 @@
-import React from "react";
+import Link from "next/link";
 
 function Header() {
   return (
-    <header className="bg-purple text-white py-4 font-roboto flex justify-center items-center">
-      <div className="text-2xl font-bold tracking-[.25em]	">TMDB</div>
-      <div className="rounded-3xl bg-white w-[80px] h-[24px]"></div>
+    <header className="bg-purple text-white py-4 font-roboto">
+      <Link passHref href="/">
+        <div className="flex justify-center items-center">
+          <div className="text-2xl font-bold tracking-[.25em]	">TMDB</div>
+          <div className="rounded-3xl bg-white w-[80px] h-[24px]"></div>
+        </div>
+      </Link>
     </header>
   );
 }
